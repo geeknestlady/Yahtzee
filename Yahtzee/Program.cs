509660b458fedc937a94e3d4e762e0bad31aadc4
carrieheyen@gmail.com <carrieheyen@gmail.com>
2019-03-08 12:55:29 -0600

    A start

--- Yahtzee/Program.cs
+++ Yahtzee/Program.cs
@@ -6,7 +6,30 @@ namespace Yahtzee
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+          
+            int[] diceRolls = new int[6];
+            diceRolls = RandomDiceRoll(diceRolls);
+
+            for (int i = 0; i < diceRolls.Length; i++)
+            {
+                Console.WriteLine("Dice {0} = {1}", i, diceRolls[i]);
+            }
+           
+            Console.ReadLine();
+        }
+        public static int[] RandomDiceRoll (int[] diceRolls) 
+        {
+            diceRolls = new int[6];
+            for (int i = 0; i < diceRolls.Length; i++)
+            {
+                int randomDice = new Random().Next(1, 6);
+                int dice = randomDice;
+                diceRolls = new int[] { dice };
+                diceRolls[i] = dice;
+                
+
+            }
+            return diceRolls;
         }
     }
 }
