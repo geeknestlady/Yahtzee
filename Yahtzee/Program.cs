7f51d5dd86042ea34e03aa49ed67fd685b7711c4
carrieheyen@gmail.com <carrieheyen@gmail.com>
2019-03-17 14:33:17 -0500

    breakthrough getting the dice rolls to stay

--- Yahtzee/Program.cs
+++ Yahtzee/Program.cs
@@ -13,53 +13,82 @@ namespace Yahtzee
                 yahtzeeMenu = YahtzeeMenu();
                 int[] diceRolls = new int[5];
                 diceRolls = RandomDiceRoll(diceRolls);
+                string diceName = "ABCDE";
+                char[] diceNameArray = diceName.ToCharArray();
+
+                Console.WriteLine("Using commas in between, type 0 to reroll the dice or type the dice's value to keep that dice");
+                Console.WriteLine("For example, if you rolled 5,6,2,2,1 and wanted to keep the 2s, type 0,0,2,2,0");
 
                 for (int i = 0; i < diceRolls.Length; i++)
                  {
-                    Console.WriteLine("Dice {0} = {1}", i, diceRolls[i]);
+                    Console.WriteLine("Dice {0} = {1}", diceNameArray[i], diceRolls[i]);
                    
                  }
-                Console.WriteLine("Which dice do you want to reroll?");
-                Console.WriteLine("If there are multiple dice use a comma");
+                Console.WriteLine("Which dice do you want to keep and which do you want to reroll?");                
                 string keptDiceInput = Console.ReadLine();
-                string[] keptDiceString = keptDiceInput.Split(',');
-                int[] keptDiceInt = new int[keptDiceString.Length];
-                for (int j = 0; j < keptDiceInt.Length; j++)
-                 {
-                     
-                         if (diceRolls[j] != keptDiceInt[j])
-                         {
-                             diceRolls[j] = Dice();
-                         }
-                        else
-                        {
-                            diceRolls[j] = diceRolls[j];
-                        }
+
+                string[] keptDiceStringArray = keptDiceInput.Split(',');
+                int[] keptDiceInt = new int[keptDiceStringArray.Length];
+                for (int i = 0; i < keptDiceStringArray.Length; i++)
+                {
+                    int number = Convert.ToInt32(keptDiceStringArray[i]);
+                    keptDiceInt[i] = number;
                 }
                 
-              
+                int[] secondDiceRolls = new int[diceRolls.Length];
                 for (int i = 0; i < diceRolls.Length; i++)
                 {
-                    Console.WriteLine("Dice {0} = {1}", i, diceRolls[i]);
+                  int number;
+                    if (diceRolls[i] == keptDiceInt[i])
+                   {
+                     number = keptDiceInt[i];
+                            
+                   }
+                   else
+                   {
+                     number = Dice();
+                   }
+                                          
+                    secondDiceRolls[i] = number;
                 }
-                Console.WriteLine("Which dice do you want to reroll?");
-                Console.WriteLine("If there are multiple dice use a comma");
-                keptDiceInput = Console.ReadLine();
-                keptDiceString = keptDiceInput.Split(',');
-                keptDiceInt = new int[keptDiceString.Length];
-                for (int j = 0; j < keptDiceInt.Length; j++)
+                for (int i = 0; i < secondDiceRolls.Length; i++)
                 {
+                    Console.WriteLine("Dice {0} = {1}", i, secondDiceRolls[i]);
 
-                    if (diceRolls[j] != keptDiceInt[j])
-                    {
-                        diceRolls[j] = Dice();
-                    }
-                                      
                 }
-                for (int i = 0; i < diceRolls.Length; i++)
+                Console.WriteLine("Which dice do you want to keep and which do you want to reroll?");
+                string keptDiceInputTwo = Console.ReadLine();
+
+                string[] keptDiceStringArrayTwo = keptDiceInput.Split(',');
+                int[] keptDiceIntTwo = new int[keptDiceStringArrayTwo.Length];
+                for (int i = 0; i < keptDiceStringArrayTwo.Length; i++)
                 {
-                    Console.WriteLine("Dice {0} = {1}", i, diceRolls[i]);
+                    int number = Convert.ToInt32(keptDiceStringArrayTwo[i]);
+                    keptDiceIntTwo[i] = number;
                 }
+
+                int[] thirdDiceRolls = new int[secondDiceRolls.Length];
+                for (int i = 0; i < secondDiceRolls.Length; i++)
+                {
+                    int number;
+                    if (secondDiceRolls[i] == keptDiceInt[i])
+                    {
+                        number = keptDiceInt[i];
+
+                    }
+                    else
+                    {
+                        number = Dice();
+                    }
+
+                    thirdDiceRolls[i] = number;
+                }
+                for (int i = 0; i < thirdDiceRolls.Length; i++)
+                {
+                    Console.WriteLine("Dice {0} = {1}", i, thirdDiceRolls[i]);
+
+                }
+
 
                 Console.ReadLine();
              }
@@ -68,7 +97,7 @@ namespace Yahtzee
         {            
             for (int i = 0; i < 5; i++)
             {
-                int randomDice = new Random().Next(1, 6);
+                int randomDice = new Random().Next(1, 7);
                 int dice = randomDice;                
                 diceRolls[i] = dice;              
 
@@ -77,7 +106,7 @@ namespace Yahtzee
         }
         public static int Dice()
         {
-            int randomNum = new Random().Next(1, 6);
+            int randomNum = new Random().Next(1, 7);
             int dice = randomNum;
             return dice;
         }
